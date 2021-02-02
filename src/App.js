829fc69233ba6631f63d72a6c2a5ef14b0411af2
Hoàng Tiến <hoangtien@huongnghiepaau.com>
2021-02-02 13:56:44 +0700

    edit title page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Recipes from './components/Recipes';
 import { Provider } from 'react-redux';
 import store from './store'
 
 function App() {
-  
+  useEffect(() => {
+    document.title = "React Recipe App"
+  }, [])
   return (
     <Provider store={store}>    
           <Recipes />
