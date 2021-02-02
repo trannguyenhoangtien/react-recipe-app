d8c24503010ac9052cf3e9bea9372220b09777fe
Hoàng Tiến <hoangtien@huongnghiepaau.com>
2021-02-02 12:32:24 +0700

    add InfiniteScroll, get more data

--- src/actions/recipeActions.js
+++ src/actions/recipeActions.js
@@ -3,11 +3,20 @@ import { FETCH_RECIPES } from "../types";
 const APP_ID = "54b6ab56";
 const APP_KEY = "9f7f110706c0aeb6897c075f6c270f7e";
 export const fetchRecipes = (query, from, to) => async (dispatch) =>{
-    const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${from}&to=${to}`);
-    const data = await res.json();
+    let data = null;
+    let error = null;
+    const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${from}&to=${to}`)
+    .catch((e) =>{
+        error = "Bad request from server, please try again later.";
+    });
     
+    if(res){
+        data = await res.json();
+        console.log(data);
+    }
+
     dispatch({
-        payload: data.hits,
+        payload: {items: data===null? null : data.hits, count: data===null?0:data.count + 1, error: error},
         type: FETCH_RECIPES
     })
 };
